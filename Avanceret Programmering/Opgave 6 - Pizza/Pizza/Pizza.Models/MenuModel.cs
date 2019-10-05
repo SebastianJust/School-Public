77e92b46fc06a63702a2ed1e778670532a64e737
Sebastian Just Mikkelsen <sjm@aros-forsikring.dk>
2019-10-06 01:18:54 +0200

    Started up with menu application due to pizza

---
+++ Avanceret Programmering/Opgave 6 - Pizza/Pizza/Pizza.Models/MenuModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pizza.Models
+{
+    public class MenuModel
+    {
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public List<ProductModel> Product { get; set; }
+
+    }
+}
---
+++ Avanceret Programmering/Opgave 6 - Pizza/Pizza/Pizza.Models/ProductModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizza.Models
+{
+   public class ProductModel
+    {
+        /// <summary>
+        /// Defines the category
+        /// </summary>
+        public Guid Category { get; set; }
+        public string Number { get; set; }
+        /// <summary>
+        /// Main title
+        /// </summary>
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
