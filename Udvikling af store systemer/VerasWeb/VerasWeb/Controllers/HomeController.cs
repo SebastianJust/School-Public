b713f6c4f3128931b67845f0a4834955c0a4352e
Sebastian Just Mikkelsen <sjm@aros-forsikring.dk>
2020-09-10 01:13:55 +0200

    Started up

---
+++ Udvikling af store systemer/VerasWeb/VerasWeb/Controllers/HomeController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VerasWeb.Infrastructure.ErrorHandling;
+using VerasWeb.Models;
+using VerasWeb.Models.Identity;
+
+namespace VerasWeb.Controllers
+{
+    //TODO: Add Authorize
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [ImportModelState]
+        [HttpGet("/customers")]
+        public async Task<IActionResult> Customers()
+        {
+            //TODO: Return all customers
+
+
+            return View(new ProfileViewModel
+            {
+                Username = "user.UserName",
+                Email = "user.Email",
+                FullName = "user.FullName"
+            });
+        }
+
+        [ImportModelState]
+        [HttpGet("/customer")]
+        public async Task<IActionResult> Customer(string cprNumber)
+        {
+            //TODO: Search for customer by CPR and return data from COSMO DB
+
+       
+            return View(new ProfileViewModel
+            {
+                Username = "user.UserName",
+                Email = "user.Email",
+                FullName = "user.FullName"
+            });
+        }
+
+        [Authorize(Roles = "Admin")]
+        [ExportModelState]
+        [HttpPost("/customer")]
+        public async Task<IActionResult> UpdateProfile(
+            [FromForm]
+            ProfileViewModel input)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Customer));
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var email = await _userManager.GetEmailAsync(user);
+            if (input.Email != email)
+            {
+                var setEmailResult = await _userManager.SetEmailAsync(user, input.Email);
+                if (!setEmailResult.Succeeded)
+                {
+                    foreach (var error in setEmailResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+
+            // Model state might not be valid anymore if we weren't able to change the e-mail address
+            // so we need to check for that before proceeding
+            if (ModelState.IsValid)
+            {
+                if (input.FullName != user.FullName)
+                {
+                    // If we receive an empty string, set a null full name instead
+                    user.FullName = string.IsNullOrWhiteSpace(input.FullName) ? null : input.FullName;
+                }
+
+                await _userManager.UpdateAsync(user);
+
+                await _signInManager.RefreshSignInAsync(user);
+
+                StatusMessage = "Your profile has been updated";
+            }
+
+            return RedirectToAction(nameof(Customer));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
