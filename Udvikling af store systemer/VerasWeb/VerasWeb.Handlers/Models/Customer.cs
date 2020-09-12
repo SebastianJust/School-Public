4510cdf1a07932a2896890e0a057b387c3d27526
Sebastian Just Mikkelsen <sjm@aros-forsikring.dk>
2020-09-12 04:12:51 +0200

    Added more names

--- Udvikling af store systemer/VerasWeb/VerasWeb.Handlers/Models/Customer.cs
+++ Udvikling af store systemer/VerasWeb/VerasWeb.Handlers/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using Newtonsoft.Json;
 
@@ -7,7 +8,7 @@ namespace VerasWeb.Handlers.Models
 {
     public class Customer
     {
-
+        [StringLength(10, ErrorMessage = "The {0} must be at max {1} characters long.")]
         [JsonProperty(PropertyName = "cprNumber")]
         public string CprNumber { get; set; }
 
